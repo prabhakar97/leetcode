50124f9b14500b68be5e38a4c9aabb8c82dd94dc
Prabhakar Kumar <prabhakar97@gmail.com>
2018-09-10 20:56:48 +0530

    Solve 107 level order from bottom

---
+++ src/L107.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+public class L107 {
+    /**
+     * 107. Binary Tree Level Order Traversal II https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+     *
+     * @timeComplexity O(n)
+     * @spaceComplexity O(n)
+     */
+    static class Solution {
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            if (root == null) {
+                return new ArrayList<>();
+            }
+            Map<Integer, List<Integer>> result = new HashMap<>();
+            Map<TreeNode, Integer> levels = new HashMap<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            levels.put(root, 0);
+            while (!queue.isEmpty()) {
+                TreeNode current = queue.poll();
+                if (current == null) {
+                    continue;
+                }
+                int currentLevel = levels.get(current);
+                if (!result.containsKey(currentLevel)) {
+                    result.put(currentLevel, new ArrayList<>());
+                }
+                result.get(currentLevel).add(current.val);
+                queue.add(current.left);
+                queue.add(current.right);
+                levels.put(current.left, currentLevel + 1);
+                levels.put(current.right, currentLevel + 1);
+            }
+            return result.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey, Comparator.reverseOrder())).map(Map.Entry::getValue).collect(Collectors.toList());
+        }
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(new L107.Solution().levelOrderBottom(root));
+    }
+}
